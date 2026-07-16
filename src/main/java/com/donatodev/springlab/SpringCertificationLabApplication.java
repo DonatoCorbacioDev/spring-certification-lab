8308e8120cfc99402b5319a46d9b4e59cdfeeb7f
DESKTOP-Q5CMCK9\Donato <donatocorbacio92@gmail.com>
2026-07-16 10:37:31 +0200

    Initialize Spring certification lab

---
+++ src/main/java/com/donatodev/springlab/SpringCertificationLabApplication.java
@@ -0,0 +1,13 @@
+package com.donatodev.springlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCertificationLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCertificationLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donatodev/springlab/SpringCertificationLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donatodev.springlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCertificationLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
