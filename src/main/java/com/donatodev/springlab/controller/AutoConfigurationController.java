70d3a3f8c534e11129e02a41b121abd9ef0aff38
DESKTOP-Q5CMCK9\Donato <donatocorbacio92@gmail.com>
2026-07-17 10:48:38 +0200

    Add Spring Boot auto-configuration endpoints

---
+++ src/main/java/com/donatodev/springlab/controller/AutoConfigurationController.java
@@ -0,0 +1,24 @@
+package com.donatodev.springlab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Espone informazioni dimostrative
+ * sull'auto-configurazione di Spring Boot.
+ */
+@RestController
+@RequestMapping("/api/autoconfig")
+public class AutoConfigurationController {
+
+    @GetMapping("/info")
+    public String autoConfigurationInfo() {
+        return "Spring Boot auto-configuration uses classpath, beans and properties";
+    }
+
+    @GetMapping("/web")
+    public String webAutoConfigurationInfo() {
+        return "starter-webmvc allows Spring Boot to configure a web application";
+    }
+}
